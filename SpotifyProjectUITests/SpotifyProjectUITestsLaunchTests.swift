144d76a8768451442c31a99a268e0818ba40c316
gvladislav-52 <gvladislav_52@mail.ru>
2024-07-17 15:43:10 +0300

    Initial Commit

---
+++ SpotifyProjectUITests/SpotifyProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SpotifyProjectUITestsLaunchTests.swift
+//  SpotifyProjectUITests
+//
+//  Created by gvladislav-52 on 17.07.2024.
+//
+
+import XCTest
+
+final class SpotifyProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
