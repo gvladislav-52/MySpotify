fdfb6fc25c10ce6a8fa1751e78eafd38c7fa3f37
gvladislav-52 <gvladislav_52@mail.ru>
2024-07-17 23:52:55 +0300

    Added comments

--- SpotifyProject/Managers/AuthManager.swift
+++ SpotifyProject/Managers/AuthManager.swift
@@ -8,41 +8,65 @@
 import Foundation
 
 final class AuthManager {
+    
+    // URI перенаправления, используемый в процессе OAuth
+    // Это константа, которая хранит URL перенаправления, используемый в процессе OAuth для возвращения пользователя после успешного входа.
     let redirectURI = "https://github.com/gvladislav-52"
+    
+    // Общий экземпляр AuthManager
+    // что позволяет реализовать паттерн Singleton, чтобы был только один экземпляр этого класса.
     static let shared = AuthManager()
     
+    // Константы, используемые в процессе аутентификации
+    // clientID: ID клиента, который выдается приложению при регистрации в Spotify.
+    // clientSecret: Секретный ключ клиента, который также выдается при регистрации.
+    // tokenAPIURL: URL API для получения токена.
     struct Constants {
         static let clientID = "c5283aeb39f74f97aa174dd8b8da9c16"
         static let cleintSecret = "a6f684a3472947d6a4150aa752b3a2e6"
         static let tokenAPIURL = "https://accounts.spotify.com/api/token"
     }
     
+    // Приватный инициализатор для обеспечения паттерна синглтон
     private init() {}
     
+    // Вычисляемое свойство для генерации URL для входа
+    // Здесь объявляется вычисляемое свойство signInURL, которое будет возвращать URL для входа пользователя.
+    // Сначала задаются параметры запроса:
+    //      scopes: Запрашиваемые разрешения (в данном случае доступ к личной информации пользователя).
+    //      base: Базовый URL для авторизации.
     public var signInURL: URL? {
         let scopes = "user-read-private"
-        //let redirectURI = "https://github.com/gvladislav-52"
         let base = "https://accounts.spotify.com/authorize"
+        
+        // Конструируем полный URL для страницы входа
         let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
         return URL(string: string)
     }
     
+    // Проверка, вошел ли пользователь в систему, проверяя наличие access token
     var isSignedIn: Bool {
         return accessToken != nil
     }
     
+    // Access token, хранящийся в UserDefaults
     private var accessToken: String? {
         return  UserDefaults.standard.string(forKey: "access_token")
     }
     
+    // Refresh token, хранящийся в UserDefaults
     private var refreshToken: String? {
         return  UserDefaults.standard.string(forKey: "refresh_token")
     }
     
+    // Дата истечения токена, хранящаяся в UserDefaults
     private var tokenExpirationDate: Date? {
         return UserDefaults.standard.object(forKey: "expirationDate") as? Date
     }
     
+    // Проверка, нужно ли обновлять токен (если он истекает в течение следующих 5 минут)
+    // Этот метод проверяет, нужно ли обновлять токен, проверяя, истекает ли он в течение следующих 5 минут.
+    // Если дата истечения доступна и текущая дата плюс пять минут больше или равна дате истечения, возвращается true.
     private var shouldRefreshToken: Bool {
         guard let expirationDate = tokenExpirationDate else { return false }
         let currentDate = Date()
@@ -50,10 +74,14 @@ final class AuthManager {
         return currentDate.addingTimeInterval(fiveMinutes) >= expirationDate
     }
     
+    // Обмен кода авторизации на access token
+    // Объявляется публичный метод для обмена кода авторизации на access token. Он принимает код и замыкание для обработки результата.
     public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
-        //GET TOKEN
+        // Получаем URL для токена
         guard let url = URL(string: Constants.tokenAPIURL) else { return }
         
+        // Подготавливаем компоненты URL с необходимыми параметрами запроса
+        // Создаем URLComponents и добавляем необходимые параметры запроса: grant_type (тип запроса), code (код авторизации) и redirect_uri (URI перенаправления).
         var components = URLComponents()
         components.queryItems = [
             URLQueryItem(name: "grant_type", value: "authorization_code"),
@@ -61,30 +89,36 @@ final class AuthManager {
             URLQueryItem(name: "redirect_uri", value: redirectURI)
         ]
         
+        // Создаем POST запрос для получения токена
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         request.httpBody = components.query?.data(using: .utf8)
         
-        let basicToken = Constants.clientID+":"+Constants.cleintSecret
+        // Создаем заголовок Basic authorization
+        let basicToken = Constants.clientID + ":" + Constants.cleintSecret
         let data = basicToken.data(using: .utf8)
-        guard let base64String = data?.base64EncodedString() else { 
+        guard let base64String = data?.base64EncodedString() else {
             print("Failure to get base64")
             completion(false)
             return
         }
          
-        
+        // Добавляем заголовок авторизации к запросу
         request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
         
+        // Выполняем запрос для обмена кода на токен
         let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
-            guard let data = data, error == nil else { completion(false); return }
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
             
             do {
-                //let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                // Декодируем ответ в объект AuthResponse
                 let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                // Кэшируем данные токена
                 self?.cacheToken(result: result)
-                //print("SUCCESS: \(json)")
                 completion(true)
             } catch {
                 print(error.localizedDescription)
@@ -94,13 +128,15 @@ final class AuthManager {
         task.resume()
     }
     
+    // Кэшируем данные токена в UserDefaults
     public func cacheToken(result: AuthResponse) {
         UserDefaults.standard.setValue(result.access_token, forKey: "access_token")
         UserDefaults.standard.setValue(result.refresh_token, forKey: "refresh_token")
         UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)), forKey: "expirationDate")
     }
     
+    // Функция для обновления access token (еще не реализована)
     public func refreshAccessToken() {
-        
+        //ТУТ ОСТАНОВИЛСЯ
     }
 }
